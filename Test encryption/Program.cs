a8cd5e317d91532d61ff786bca3491d75ee8cec6
Anthony Victor <pe27igm@eduvaud.ch>
2023-11-01 15:40:03 +0100

    création rep csharp

---
+++ Test encryption/Program.cs
@@ -0,0 +1,84 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
+try
+{
+    using (FileStream fileStream = new("TestData.txt", FileMode.OpenOrCreate))
+    {
+        using (Aes aes = Aes.Create())
+        {
+            byte[] key =
+            {
+                0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
+            };
+            aes.Key = key;
+
+            byte[] iv = aes.IV;
+            fileStream.Write(iv, 0, iv.Length);
+
+            using (CryptoStream cryptoStream = new(
+                fileStream,
+                aes.CreateEncryptor(),
+                CryptoStreamMode.Write))
+            {
+                
+                Console.WriteLine("enter something to encrypt: ");
+                string entry = Console.ReadLine();
+                using (StreamWriter encryptWriter = new(cryptoStream))
+                {
+                    encryptWriter.WriteLine(entry);
+                }
+
+
+             
+            }
+        }
+    }
+
+    Console.WriteLine("The file was encrypted.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"The encryption failed. {ex}");
+}
+
+
+
+
+try
+{
+    using (FileStream fileStream = new("TestData.txt", FileMode.Open))
+    {
+        using (Aes aes = Aes.Create())
+        {
+            byte[] key =
+            {
+                0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
+            };
+            aes.Key = key;
+
+            byte[] iv = new byte[16];
+            fileStream.Read(iv, 0, iv.Length);
+            aes.IV = iv;
+
+            using (CryptoStream cryptoStream = new(
+                fileStream,
+                aes.CreateDecryptor(),
+                CryptoStreamMode.Read))
+            {
+                using (StreamReader decryptReader = new(cryptoStream))
+                {
+                    string decryptedText = decryptReader.ReadLine();
+                    Console.WriteLine("Decrypted Text: " + decryptedText);
+                }
+            }
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"The decryption failed. {ex}");
+}
+
---
+++ rubiks1/rubiks1/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string cube00 = "VertRougeBleu";
+string cube01 = "VertBlanc";
+string cube02 = "RougeBlancBleu";
+string cube03 = "BleuOrange";
+string cube04 = "Blanc";
+string cube05 = "VertOrange";
+string cube06 = "RougeVertBlanc";
+string cube07 = "RougeBleu";
+string cube08 = "BlancOrangeVert";
+
+//recup un nb aléatoire entre 0 et 8
+Random random = new Random();
+int randomCube = random.Next(9);
+
+//Associe le cube avec le num aléatoire
+string selectedCube = ;
+if (randomCube == 0)
+{
+    selectedCube = cube00;
+}
+else if (randomCube == 1)
+{
+    selectedCube = cube01;
+}
+else if (randomCube == 2)
+{
+    selectedCube = cube02;
+}
+else if (randomCube == 3)
+{
+    selectedCube = cube03;
+}
+else if (randomCube == 4)
+{
+    selectedCube = cube04;
+}
+else if (randomCube == 5)
+{
+    selectedCube = cube05;
+}
+else if (randomCube == 6)
+{
+    selectedCube = cube06;
+}
+else if (randomCube == 7)
+{
+    selectedCube = cube07;
+}
+else if (randomCube == 8)
+{
+    selectedCube = cube08;
+}
+
+//ask user to guess color
+Console.Write("A votre avis quelle est la couleur du cube " + randomCube + " :");
+string answer = Console.ReadLine();
+if (answer == selectedCube)
+{
+    Console.WriteLine("Bravo! Vous avez trouvé!");
+}
+else
+{
+    Console.WriteLine("Désolé, votre réponse est incorrecte.");
+
+}
